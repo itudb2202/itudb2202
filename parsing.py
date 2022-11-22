115d54c26061972d3252885f820e393dcba3e208
Pelinsu Şahin <sahinpelinsu7@gmail.com>
2022-11-23 00:08:14 +0300

    Parsing update

--- parsing.py
+++ parsing.py
@@ -75,4 +75,25 @@ for inr in data.index:
     
 
 
+#       ----------KICKOFF---------
+data = pd.read_csv("Career_Stats_Kickoff.csv")
+column_names = ["Player Id", "Year", "Team", "Games Played", "Kickoffs", "Kickoff Yards",
+                "Out of Bounds Kickoffs","Yards Per Kickoff","Touchbacks"]
+for data_column in data.columns:
+    if data_column not in column_names:
+        data.drop(data_column, inplace = True, axis=1)
+for inr in data.index:
+    row = data.iloc[inr]
+    insertValues = ""
+    column_index = 0
+    for column_name in column_names:
+        if (column_index != len(column_names) - 1):
+            insertValues = insertValues + str(row[column_name])+","
+        else:
+            insertValues = insertValues + str(row[column_name])
+        column_index = column_index + 1
+    statement = "INSERT INTO career_stats_kickoff VALUES (%s)" % insertValues
+    
+
+
    
