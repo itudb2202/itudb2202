bf8bb3decc61c1acc8891617195ab02e04263d17
Merve İnal <merve.inall16@gmail.com>
2022-11-22 21:31:01 +0300

    Parsing updated.

--- parsing.py
+++ parsing.py
@@ -12,6 +12,25 @@ for inr in data.index:
             insertValues = insertValues + str(row[column_name])
         column_index = column_index + 1
     statement = "INSERT INTO basic_stats VALUES (%s)" % insertValues
+    
+
+data = pd.read_csv("Career_Stats_Defensive.csv")
+column_names = ["Player Id", "Year", "Team", "Games Played", "Total Tackles", "Solo Tackles",
+                "Assisted Tackles","Passes Defended","Ints","Yards Per Int"]
+for data_column in data.columns:
+    if data_column not in column_names:
+        data.drop(data_column, inplace = True, axis=1)
+for inr in data.index:
+    row = data.iloc[inr]
+    insertValues = ""
+    column_index = 0
+    for column_name in column_names:
+        if (column_index != len(column_names) - 1):
+            insertValues = insertValues + str(row[column_name])+","
+        else:
+            insertValues = insertValues + str(row[column_name])
+        column_index = column_index + 1
+    statement = "INSERT INTO career_stats_defensive VALUES (%s)" % insertValues
 
 
 data = pd.read_csv("Career_Stats_Receiving.csv")
