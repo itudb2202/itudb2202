33c8ed2ed28702810d767a21f3cf5fd22c2b1a12
Salih Bora Öztürk <salihboraozturk@Boras-MacBook-Air.local>
2022-11-23 04:23:55 +0300

    add section for passing

--- parsing.py
+++ parsing.py
@@ -95,5 +95,23 @@ for inr in data.index:
     statement = "INSERT INTO career_stats_kickoff VALUES (%s)" % insertValues
     
 
+# ---------- PASSSING ---------
+data = pd.read_csv("Career_Stats_Passing.csv")
+column_names = ["Player Id", "Year", "Team", "Games Played", "Passes Attempted", "Passes Completed",
+                "Completion Percentage","Passer Rating"]
+for data_column in data.columns:
+    if data_column not in column_names:
+        data.drop(data_column, inplace = True, axis=1)
+for inr in data.index:
+    row = data.iloc[inr]
+    insertValues = ""
+    column_index = 0
+    for column_name in column_names:
+        if (column_index != len(column_names) - 1):
+            insertValues = insertValues + str(row[column_name])+","
+        else:
+            insertValues = insertValues + str(row[column_name])
+        column_index = column_index + 1
+    statement = "INSERT INTO career_stats_passing VALUES (%s)" % insertValues
+    
 
-   
