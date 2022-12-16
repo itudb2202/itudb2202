5bc476654f686460a7771431bec5d3213b81119d
borawhocodess <borawhocodess@gmail.com>
2022-12-16 04:14:20 +0300

    add show func

--- proje/views.py
+++ proje/views.py
@@ -3,32 +3,31 @@ from datetime import datetime
 from flask import current_app
 from database import Database
 
+
 def home_page():
     today = datetime.today()
     day_name = today.strftime("%A")
     return render_template("home.html", day=day_name)
-    
+
 def buse_page():
     db = current_app.config["dbconfig"]
     punting = db.get_all_punting_stats()
     return render_template("buse.html", punting_db = punting)
-
 
 def merve_page():
     db = current_app.config["dbconfig"]
     defensive = db.get_all_defensive_stats()
     return render_template("merve.html", defensive_db = defensive)
 
-
 def pelin_page():
     db = current_app.config["dbconfig"]
     kickoff = db.get_all_kickoff_stats()
     return render_template("pelin.html", kickoff_db = kickoff)
 
-
 def bora_page():
-    return render_template("bora.html")
-
+    db = current_app.config["dbconfig"]
+    passing = db.get_all_passing_stats()
+    return render_template("bora.html", passing_db=passing)
 
 def atacan_page():
     db = current_app.config["dbconfig"]
